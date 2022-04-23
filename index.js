b874ef251f7ea42b8ef536ecfb4c8717483d0f5c
seekerForKnowlegde <89083692+seekerForKnowlegde@users.noreply.github.com>
2022-04-23 13:13:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connect to db")
+);
+
+app.listen(4000, () => {
+  console.log("server up and running on port 4000");
+});
